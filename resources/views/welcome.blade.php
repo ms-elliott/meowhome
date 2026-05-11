50aa6e2e835d7be232454406a66dbb820a6a26b5
ms-elliott <ms.elliott55@gmail.com>
2026-05-11 23:38:57 +0900

    Refactoring part 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,37 +1,44 @@
 @extends('layouts.common')
+
 @section('title', 'MEOW HOME')
+
 @section('content')
-<div class="text-center">
-    @if(session('message'))
-    <div class="row d-flex justify-content-center">
-        <div class="col-12 col-md-5">
-            <div class="mt-2 mb-3 px-3 py-1 alert alert-primary">
-                <p class="my-auto">{{ session('message') }}</p>
-            </div>
-        </div>
+<div class="welcome-page">
+
+    {{-- フラッシュメッセージ --}}
+    @if (session('message'))
+    <div class="flash-message flash-message--success welcome-page__flash">
+        <p>{{ session('message') }}</p>
     </div>
     @endif
-    <div>
-        <img src="{{ asset('mh_images/meowhome_image.png')}}" alt="MeowHomeイメージ" height="300" class="mt-3">
+
+    {{-- メインビジュアル --}}
+    <div class="welcome-hero">
+        <img
+            src="{{ asset('mh_images/meowhome_image.png') }}"
+            alt="MeowHomeイメージ"
+            class="welcome-hero__image">
+        <img
+            src="{{ asset('mh_images/meowhome_logo.png') }}"
+            alt="MeowHomeロゴ"
+            class="welcome-hero__logo">
+        <h3 class="welcome-hero__catch">あたらしいおうちへ、ただいま。</h3>
+        <h5 class="welcome-hero__sub">ー 猫専用 譲渡マッチングアプリ ー</h5>
     </div>
-    <div>
-        <img src="{{ asset('mh_images/meowhome_logo.png')}}" alt="MeowHomeロゴ" height=50" class="mb-5">
-    </div>
-    <h3 class="ms-3">あたらしいおうちへ、ただいま。</h3>
-    <h5>ー 猫専用 譲渡マッチングアプリ ー</h5>
-    <div class="row row-cols-auto justify-content-center" style="margin-top: 60px;">
-        <div class="col-xl-5" style="max-width: 600px;">
-            <div class="card text-center">
-                <div class="card-body m-plus-rounded-1c-regular mx-auto justify-content-center">
-                    <h5 class="card-title mt-1">ユーザー登録はこちら</h5>
-                    <p class="card-text">累計里親募集件数：{{ App\Models\Post::ComulativePostTotal() }}件</p>
-                    <div>
-                        <a href="{{ route('users.create') }}" class="button btn btn-secondary btn-md-lg me-3 me-xl-5 mb-3">里親を募集したい方</a>
-                        <a href="{{ route('users.create') }}" class="button btn btn-secondary btn-md-lg mb-3"> 里親になりたい方 </a>
-                    </div>
-                </div>
+
+    {{-- 登録カード --}}
+    <div class="welcome-card-wrap">
+        <div class="welcome-card">
+            <h5 class="welcome-card__title">ユーザー登録はこちら</h5>
+            <p class="welcome-card__count">
+                累計里親募集件数：{{ App\Models\Post::ComulativePostTotal() }}件
+            </p>
+            <div class="welcome-card__actions">
+                <a href="{{ route('users.create') }}" class="btn btn-secondary">里親を募集したい方</a>
+                <a href="{{ route('users.create') }}" class="btn btn-secondary">里親になりたい方</a>
             </div>
         </div>
     </div>
+
 </div>
 @endsection
